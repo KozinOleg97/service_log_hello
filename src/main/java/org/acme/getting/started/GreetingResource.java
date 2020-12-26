6b9e8e4f2d592a9ed73a05d4bd792f81068830b5
KozinOleg97 <olegkosin.dev@yandex.ru>
2020-12-26 09:56:06 +0300

    service with hello/hello % hello/log

---
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -0,0 +1,31 @@
+package org.acme.getting.started;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jboss.logging.Logger;
+
+@Path("/hello")
+public class GreetingResource {
+    private static final Logger LOG = Logger.getLogger(GreetingResource.class);
+
+    int i = 0;
+
+    @GET
+    @Path("/hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        i++;
+        return " Hello RESTEasy_" + i;
+    }
+
+    @GET
+    @Path("log")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String log() {
+        LOG.info("Some useful log message");
+        return " LOG RESTEasy_" + i;
+    }
+}
