f0449e9279efc0af1f7111c10b021cd8765d78f2
KozinOleg97 <olegkosin.dev@yandex.ru>
2021-01-18 10:37:59 +0300

    metrics to microprofile

--- src/main/java/org/acme/getting/started/GreetingResource.java
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -7,8 +7,10 @@ import javax.ws.rs.core.MediaType;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.quarkus.security.Authenticated;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Gauge;
 import org.jboss.logging.Logger;
-
 
 
 @Path("/api")
@@ -17,12 +19,15 @@ public class GreetingResource {
     private static final Logger LOG = Logger.getLogger(GreetingResource.class);
 
     int i = 0;
+    private long qwe = 228;
 
     @GET
     @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
+    @Counted(name = "helloCals", description = "Hello call count.")
     public String hello() {
         i++;
+        qwe = qwe - 1;
         return " Hello RESTEasy_" + i;
     }
 
@@ -32,5 +37,10 @@ public class GreetingResource {
     public String log() {
         LOG.info("Some useful log message");
         return " LOG RESTEasy_" + i;
+    }
+
+    @Gauge(name = "test", unit = MetricUnits.NONE, description = "228.")
+    public Long test() {
+        return (long) qwe;
     }
 }
